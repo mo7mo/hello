9bed7894f4a27917cbc987b86e1c9cb285ca2543
mo7mo <microfly@microflydeiMac.local>
2015-01-04 19:12:19 +0800

    上传工程
    哈哈哈

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by microfly on 15/1/4.
+//  Copyright (c) 2015年 MicroFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
